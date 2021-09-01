5f0d854b65ac4432d4e2bcb0ea59e9c75755132f
Vinod Kumar Kotha <vk21125@corp.ojas-it.com>
2021-09-01 12:35:26 +0530

    database updated

--- Skill_Test/Skill_Test/Skill_Test/settings.py
+++ Skill_Test/Skill_Test/Skill_Test/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'Skill_Test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pocdatabase',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
+
 }
 
 
