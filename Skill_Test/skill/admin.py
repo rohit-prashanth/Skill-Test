c4fd6e61a0e26815cbfdcd95b52e77997973c6d0
SwethaGudla <81808738+SwethaGudla@users.noreply.github.com>
2021-09-02 17:55:54 +0530

    email and questions updated

---
+++ Skill_Test/skill/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+@admin.register(QuestionBank)
+class QuestionBankAdmin(admin.ModelAdmin):
+    list_display = ['question_id','question','difficulty_names','created_date','modified_date','flag']
+
+
+@admin.register(OptionsTable)
+class OptionTableAdmin(admin.ModelAdmin):
+    list_display = ['option_id','option1','option2','option3','option4','correct_option','created_date','modified_date']
+
+@admin.register(ExamResults)
+class ExamResultsAdmin(admin.ModelAdmin):
+    list_display = ['exam_id','cad_id','given_by','given_date','flag']
+
+@admin.register(TestLinkTable)
+class TestLinkTableAdmin(admin.ModelAdmin):
+    list_display = ['test_id','category_id','no_of_questions','no_of_easy_questions','no_of_medium_questions','no_of_hard_questions','date_of_exam','start_time','end_time','test_link','flag']
+
+
+@admin.register(DomainCategory)
+class DomainCategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_id','category_name']
+
+@admin.register(DifficultyLevel)
+class DiffucultyLevelAdmin(admin.ModelAdmin):
+    list_display = ['difficulty_id','difficulty_name']
+
+@admin.register(CandidatesTable)
+class CandidatesTableAdmin(admin.ModelAdmin):
+    list_display = ['candidate_id','candidate_name','domain','flag']
+
+@admin.register(EmployeeTable)
+class EmployeeTableAdmin(admin.ModelAdmin):
+    list_display = ['emp_id', 'emp_name','domain','flag']
+
+
+
+
+
+
---
+++ Skill_Test/skill/views.py
@@ -0,0 +1,73 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import QuestionBankForm,OptionTableForm
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
+def questions(request):
+    if request.method == 'POST':
+        fm = QuestionBankForm(request.POST)
+        fm1 = OptionTableForm(request.POST)
+        if fm.is_valid() and fm1.is_valid():
+            fm.save()
+            fm1.save()
+            return HttpResponseRedirect('/ques')
+        else:
+            messages.error(request,'Invalid Data')
+            return HttpResponseRedirect('/ques')
+    else:
+        fm = QuestionBankForm()
+        fm1 = OptionTableForm()
+        return render(request,'questions.html',{'form1':fm1,'form':fm})
+
+def user_login(request):
+
+   if request.method == 'POST':
+        fm = AuthenticationForm(request=request,data=request.POST)
+        print(fm.is_valid())
+        if fm.is_valid():
+            uname = fm.cleaned_data['username']
+            upass = fm.cleaned_data['password']
+            print(uname,upass)
+            user = authenticate(username=uname,password=upass)
+            print(user)
+            if user is not None:
+                login(request,user)
+                messages.success(request,'Logged in Succesfully !!!')
+                return HttpResponseRedirect('/profile')
+   else:
+        fm = AuthenticationForm()
+   return render(request, 'user_login.html', {'form': fm})
+
+def user_profile(request):
+    if request.user.is_authenticated:
+        return render(request,'profile.html',{'name':request.user})
+    else:
+        return HttpResponseRedirect('/login')
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login')
+
+def Send_link_to_Email(request):
+    if request.method == 'POST':
+        mails = request.POST.get('email')
+        var = mails.split(',')
+        print(var)
+        for i in var:
+            # to = request.POST.get('email')
+            # print(to)
+            content = request.POST.get('content')
+            print(i, content)
+            send_mail(
+                "Skill Assessment | OILC - 301 | Django | 30-Aug-21",
+                content,
+                settings.EMAIL_HOST_USER,
+                [i]
+            )
+        return render(request, 'index.html', {'name': 'SEND eMAIL NOTIFICATION'})
+    else:
+        return render(request, 'index.html', {'name': 'SEND eMAIL NOTIFICATION'})
